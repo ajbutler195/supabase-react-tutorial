2c32125ed64c6997b7579c46fdf92d13f74a2c91
AJ Butler <ajbutler195@gmail.com>
2023-01-06 15:42:22 -0500

    finished, working code.

--- to-do/src/components/ClearTasks.js
+++ to-do/src/components/ClearTasks.js
@@ -1,9 +1,22 @@
 import { Flex, Button } from '@chakra-ui/react';
+import { useState } from 'react';
+import supabase from '../supabase.js';
 
 export default function ClearTasks() {
+    const [loading, setLoading] = useState(false);
+
+    async function handleClear() {
+        setLoading(true);
+        const { data, error } = await supabase
+            .from('todos')
+            .delete()
+            .not('text', 'eq', 'Do Not Delete.');
+        setLoading(false);
+    }
+
     return (
         <Flex>
-            <Button colorScheme='gray' px='8' h='45' color='gray.500' mt='10'>
+            <Button colorScheme='gray' px='8' h='45' color='gray.500' mt='10' onClick={handleClear} isLoading={loading} loadingText="Clearing">
                 Clear Tasks
             </Button>
         </Flex>
--- to-do/src/components/TaskList.js
+++ to-do/src/components/TaskList.js
@@ -1,23 +1,54 @@
-import { 
-    VStack, 
-    StackDivider, 
-    HStack, 
-    Text, 
-    Image, 
-    Box 
+import {
+    VStack,
+    StackDivider,
+    HStack,
+    Text,
+    Image,
+    Box
 } from '@chakra-ui/react';
 import DeleteTask from './DeleteTask';
 import ClearTasks from './ClearTasks';
 import img from '../images/empty.svg';
+import supabase from '../supabase.js';
+import { useEffect, useState } from 'react';
 
 export default function TaskList() {
+    const [tasks, setTasks] = useState([]);
+
+    async function fetchData() {
+        let { data: tasks, error } = await supabase.from('todos').select('*');
+        setTasks(tasks);
+    }
+
+    useEffect(() => { 
+        fetchData() 
+    }, [])
+
+    useEffect(() => {
+        const todoListener = supabase.channel('custom-all-channel')
+        .on(
+        'postgres_changes',
+        { event: '*', schema: 'public', table: 'todos' },
+        (payload) => {
+            fetchData();
+            //console.log('Change received!', payload)
+        }
+        )
+        .subscribe();
+
+        return () => {
+            todoListener.unsubscribe();
+        };
+    })
 
     // If there are no tasks, show placeholder image
-    return (
-        <Box align='center'>
-            <Image src={img} mt='30px' maxW='95%' />
-        </Box>
-    );
+    if (!tasks.length) {
+        return (
+            <Box align='center'>
+                <Image src={img} mt='30px' maxW='95%' />
+            </Box>
+        );
+    }
 
     return (
         <>
@@ -29,14 +60,16 @@ export default function TaskList() {
                 borderRadius='lg'
                 w='100%'
                 maxW={{ base: '90vw', sm: '80vw', lg: '50vw', xl: '30vw' }}
-                alignItems="stretch"
+                alignItems="stretch" 
             >
-                {/* <HStack key=''>
-                    <Text w='100%' p='8px' borderRadius='lg'>
-                        Wash the dishes
-                    </Text>
-                    <DeleteTask />
-                </HStack> */}
+                {tasks.map(task => (
+                    <HStack key={task.id}>
+                        <Text w='100%' p='8px' borderRadius='lg'>
+                            {task.text}
+                        </Text>
+                        <DeleteTask id={task.id} />
+                    </HStack>
+                ))}
             </VStack>
 
             <ClearTasks />
